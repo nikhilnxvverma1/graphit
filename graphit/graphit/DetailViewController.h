8193dd598f07b72233d4fec4f3445d4683c32aea
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-01-28 23:22:18 -0500

    Committing a blank template master detail application

---
+++ graphit/graphit/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  graphit
+//
+//  Created by Nikhil Verma on 28/01/16.
+//  Copyright © 2016 Nikhil Verma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ graphit/graphit/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  graphit
+//
+//  Created by Nikhil Verma on 28/01/16.
+//  Copyright © 2016 Nikhil Verma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
